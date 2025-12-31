ca1dfa8cb4f8408aa378e8afc5d0cf093f07a72c
ruben <ruben.gallardo@inslapineda.cat>
2025-12-31 19:22:22 +0100

    implementacio del blade login y sus rutas pero sin autenticacion aun

---
+++ resources/views/login.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>App CRUD RAW</title>
+</head>
+<body>
+    <div>
+        <h1>Login</h1>
+
+        <form action="." method="post">
+            <input type="email" name="email" id="email_id" value="{{ old('email') }}">
+            <span>Email</span>
+             <br>
+            <br>
+            <input type="password" name="password" id="password_id" value="{{ old('password') }}">
+            <span>Password</span>
+            <br>
+            <br>
+            <input type="submit" value="Entrar">
+        </form>
+
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <p style="color: red">{{ $error }}</p>
+            @endforeach
+        @endif
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use App\Http\Controllers\CentroController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/menu', function () {
     return view('menu');
+});
+
+Route::get('/', function () {
+    return view('login');
 });
 
 Route::resource('centros', CentroController::class);
