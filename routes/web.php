e2eff2798ad17b05cd72d9cf2073f9d620bd2b6b
ruben <ruben.gallardo@inslapineda.cat>
2025-12-31 21:05:43 +0100

    Routes Automaticas y Manuales

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,19 @@ Route::get('/menu', function () {
 
 
 
+
 /* ************** */
 /* MY ROUTES */
 /* ************** */
+
+////// CENTROS /////
+// Automatic (php artisan route:list)
 Route::resource('centros', CentroController::class)->middleware('auth');
+// Manual 
+// Route::get('/centros', [CentroController::class, 'index'])->middleware('auth')->name('centros.index');
+// Route::get('/centros/create', [CentroController::class, 'create'])->middleware('auth')->name('centros.create');
+// Route::post('/centros', [CentroController::class, 'store'])->middleware('auth')->name('centros.store');
+// Route::get('/centros/{centro}', [CentroController::class, 'show'])->middleware('auth')->name('centros.show');
+// Route::get('/centros/{centro}/edit', [CentroController::class, 'edit'])->middleware('auth')->name('centros.edit');
+// Route::put('/centros/{centro}', [CentroController::class, 'update'])->middleware('auth')->name('centros.update');
+// Route::delete('/centros/{centro}', [CentroController::class, 'destroy'])->middleware('auth')->name('centros.destroy');
