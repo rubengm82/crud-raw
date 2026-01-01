d466f7371dfaf26e94c5a5f12f72ed5f1198e0e9
ruben <ruben.gallardo@inslapineda.cat>
2026-01-01 16:14:43 +0100

    auto-refresh when save vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [
+                'app/Http/Controllers/**',
+                'app/Models/**',
+                'resources/views/**',
+            ],
         }),
         tailwindcss(),
     ],
